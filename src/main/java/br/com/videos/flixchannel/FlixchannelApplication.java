5a0231a302341f134e4a1bc2077f7fc5edd1d15f
Daniel Diniz <daniel.diniz1@icloud.com>
2022-10-24 18:53:51 -0300

    first commit

---
+++ src/main/java/br/com/videos/flixchannel/FlixchannelApplication.java
@@ -0,0 +1,13 @@
+package br.com.videos.flixchannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixchannelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlixchannelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/videos/flixchannel/FlixchannelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.videos.flixchannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlixchannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
